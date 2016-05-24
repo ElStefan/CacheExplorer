01e72c23a32b9a9be75fb6dad197c7a9eca2604a
Stefan Lampert <das_gigabike@hotmail.com>
2016-05-24 19:44:37 +0200

    find mp3 files by createdate and merge it to a full mp3

--- CacheExplorer/Helper/CacheHelper.cs
+++ CacheExplorer/Helper/CacheHelper.cs
@@ -22,23 +22,24 @@ namespace CacheExplorer.Helper
                 return LocalPath + ChromePath;
             }
         }
+
         public static IEnumerable<CacheFile> GetFiles(bool onlyMP3)
         {
-            if(!CreateTempDir())
+            if (!CreateTempDir())
             {
                 return null;
             }
 
             var files = Directory.GetFiles(ChromeCachePath, "f_*");
             var cacheFiles = files.Select(o => new CacheFile { FilePath = o, FileName = Path.GetFileName(o), Content = File.ReadAllBytes(o), CreateDate = File.GetCreationTime(o) }).ToList();
-            if(!onlyMP3)
+            if (!onlyMP3)
             {
                 CleanupTempDir();
                 return cacheFiles;
             }
             var mp3Files = cacheFiles.AsParallel().Where(o => IsMp3(o));
             CleanupTempDir();
-            return mp3Files;
+            return FindFilesAndMerge(mp3Files.ToList());
         }
 
         private static bool CreateTempDir()
@@ -65,14 +66,12 @@ namespace CacheExplorer.Helper
         {
             foreach (var file in Directory.GetFiles(TempDir))
             {
-
                 try
                 {
                     File.Delete(file);
                 }
                 catch (Exception)
                 {
-
                     continue;
                 }
             }
@@ -83,7 +82,7 @@ namespace CacheExplorer.Helper
             var tempFileName = $"{Guid.NewGuid()}.mp3";
             var tempFile = $@"{TempDir}\{tempFileName}";
 
-            if(!Directory.Exists(TempDir))
+            if (!Directory.Exists(TempDir))
             {
                 return false;
             }
@@ -91,7 +90,11 @@ namespace CacheExplorer.Helper
             File.WriteAllBytes(tempFile, file.Content);
             var so = ShellFile.FromFilePath(tempFile);
             long nanoseconds;
-            if(long.TryParse(so.Properties.System.Media.Duration.Value.ToString(),
+            if (so.Properties.System.Media.Duration.Value == null)
+            {
+                return false;
+            }
+            if (!long.TryParse(so.Properties.System.Media.Duration.Value.ToString(),
             out nanoseconds))
             {
                 return false;
@@ -103,6 +106,61 @@ namespace CacheExplorer.Helper
                 return true;
             }
             return false;
+        }
+
+        public static List<CacheFile> FindFilesAndMerge(List<CacheFile> files)
+        {
+            var fileParts = FindFiles(files).Where(o => o.Any());
+            var mergedFiles = new List<CacheFile>();
+            foreach (var file in fileParts)
+            {
+                var mergedFile = Merge(file);
+                mergedFiles.Add(mergedFile);
+            }
+            return mergedFiles;
+        }
+
+        private static List<List<CacheFile>> FindFiles(List<CacheFile> files)
+        {
+            var foundFiles = new List<List<CacheFile>>();
+
+            var singleFile = new List<CacheFile>();
+
+            var firstFile = files.FirstOrDefault();
+            if (firstFile == null)
+            {
+                return foundFiles;
+            }
+            var currentTime = firstFile.CreateDate;
+            foreach (var item in files)
+            {
+                if (item.CreateDate <= currentTime.AddSeconds(2))
+                {
+                    singleFile.Add(item);
+                    currentTime = item.CreateDate;
+                    continue;
+                }
+                currentTime = item.CreateDate;
+                foundFiles.Add(singleFile.ToList());
+                singleFile = new List<CacheFile>();
+            }
+            if (singleFile.Any())
+            {
+                foundFiles.Add(singleFile.ToList());
+            }
+
+            return foundFiles;
+        }
+
+        private static CacheFile Merge(List<CacheFile> files)
+        {
+            var mergedFile = new CacheFile();
+            mergedFile.Content = files.SelectMany(o => o.Content).ToArray();
+            mergedFile.CreateDate = files.First().CreateDate;
+            mergedFile.FileName = $"mergedFile{mergedFile.CreateDate.ToString("yyyyMMddHHmmss")}";
+            mergedFile.FilePath = null;
+            mergedFile.Length = files.Sum(o => o.Length);
+            return mergedFile;
         }
     }
 }
